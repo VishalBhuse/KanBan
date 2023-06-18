1281f35def87091aacb182b238bc5d234657f32a
VishalBhuse <vbhuse1997@gmail.com>
2023-06-18 17:16:28 +0530

    project flow completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RootLayout from "./layouts/RootLayout";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import Message from "./Pages/Message";
 
-function App() {
+const App = () => {
+  const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RootLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/*"
+            element={<Message routeName={location.pathname} />}
+          />
+        </Routes>
+      </RootLayout>
+    </>
   );
-}
+};
 
 export default App;
